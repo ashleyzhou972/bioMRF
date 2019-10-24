616d66dc6153bd63f2d9926e0be8d6809b7d97ee
nzhou@iastate.edu <nzhou@iastate.edu>
2019-10-24 14:58:41 -0500

    move R package to separate repo

--- PhiMRF/README.md
+++
@@ -1,2 +0,0 @@
-# PhiMRF
-R package for hierarchical Markov Random Field model for analysis of spatial dependency 
--- PhiMRF/src/init.c
+++
@@ -1,9 +0,0 @@
-#include "PhiMRF.h"
-#include <R_ext/Rdynload.h>
-
-
-void R_init_PhiMRF(DllInfo *info) {
-  R_RegisterCCallable("PhiMRF", "pmrf",  (DL_FUNC) &double_metropolis_cont);
-  R_RegisterCCallable("PhiMRF", "preprocess_big",  (DL_FUNC) &preprocess);
-
-}
