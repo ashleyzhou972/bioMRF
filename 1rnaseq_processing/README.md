fd071c7ff46b55b13948e894843de9772b905e62
nzhou@iastate.edu <nzhou@iastate.edu>
2019-12-11 00:21:24 -0600

    rnaseq readme

---
+++ 1rnaseq_processing/README.md
@@ -0,0 +1,59 @@
+# Process RNA-seq data
+
+All of the following steps are in [`pipeline_rnaseq.sh`](pipeline_rnaseq.sh). Please read through each step and customize, before running
+
+`bash pipeline_rnaseq.sh`
+
+### Designate home dirctory
+
+The same directory as the [set up](../0setup/) step.
+
+Example:
+```
+homedir=/home/nzhou/hic/rao2014/GM12878_10kb/
+```
+
+## Process RNA-seq quantification file
+
+Make sure you have downloaded/prepared your RNA-seq files, according to the [set up](../0setup/) step.
+
+**Replace** the second argument with your ***raw*** RNA-seq filename, wildcards accepted.
+```
+python3 prep_rnaseq.py  ./ensembl_map_coding.txt  "$homedir/rnaseq/ENCFF*.tsv"  $homedir/rnaseq/
+```
+**Supply** the path for the ***processed*** RNA-seq file. Only one of the files is enough.
+
+Naming convention is "rnaseq_[Your raw RNA-seq BASE filename].txt".
+
+Example:
+```
+rnaseq=$homedir/rnaseq/rnaseq_ENCFF781YWT.txt
+```
+
+
+### Write to the genename folder
+
+PhiMRF should use all genes for which RNA-seq data is available. Therefore, once we've mapped all the RNA-seq data, we write all the names of the genes by chromosome to a separate folder, to inform subsequent mapping of HiC data. 
+```
+Rscript save_gene_names.R $rnaseq  $homedir/rnaseq/gene_names/
+```
+
+
+### Output RNA-seq count 
+
+This step outputs the RNA-seq count in an R matrix format, each RNA-seq file (replicate) will be one column in the matrix.
+
+Although the gene expression data are the same, we output two set of files, for intra- and inter- chromosomal datasets.
+
+More than one processed RNA-seq file can be supplied at the same time using wildcards (NO quotes around the last argument if you are using wildcard)
+
+```
+for chrmA in "${chrms[@]}"
+do
+	Rscript prep_y_for_MRF_intra.R "$chrmA" $homedir/intra/  $homedir/rnaseq/gene_names/  $homedir/rnaseq/rnaseq_*.txt
+	for chrmB in "${chrms[@]}"
+	do
+		Rscript prep_y_for_MRF_inter.R "$chrmA" "$chrmB" $homedir/inter/  $homedir/rnaseq/gene_names/  $homedir/rnaseq/rnaseq_*.txt
+	done
+done
+```
--- 1rnaseq_processing/pipeline_rnaseq.sh
+++ 1rnaseq_processing/pipeline_rnaseq.sh
@@ -15,9 +15,9 @@ echo "Please make sure you have downloaded/prepared your RNA-seq quantification 
 # The RNA-seq quantification files are downloaded from ENCODE
 echo "Step 1 Process RNA-seq quantification file..."
 ############################################################
-# replace the second argument with your RAW RNA-seq filename
 # CHANGE BELOW!!
-#python3 prep_rnaseq.py  ./ensembl_map_coding.txt  "$homedir/rnaseq/ENCFF*.tsv"  $homedir/rnaseq/
+# replace the second argument with your RAW RNA-seq filename
+python3 prep_rnaseq.py  ./ensembl_map_coding.txt  "$homedir/rnaseq/ENCFF*.tsv"  $homedir/rnaseq/
 # designate the path for the PROCESSED RNA-seq file
 # only one file is enough
 # naming convention is "rnaseq_[Your raw RNA-seq BASE filename].txt"
@@ -27,11 +27,11 @@ rnaseq=$homedir/rnaseq/rnaseq_ENCFF781YWT.txt
 
 
 ## Step 2 Write to the genename folder
-# The our model should run on all genes for which RNA-seq data is available.
+# Our model should run on all genes for which RNA-seq data is available.
 # Therefore, once we've mapped all the RNA-seq data we write all the names of the genes by chromosome to a separate folder
 # To inform subsequent mapping of HiC data
 echo "Step 2 Write gene names..."
-#Rscript save_gene_names.R $rnaseq  $homedir/rnaseq/gene_names/
+Rscript save_gene_names.R $rnaseq  $homedir/rnaseq/gene_names/
 
 
 # Step 3 Output y 
@@ -39,13 +39,13 @@ echo "Step 2 Write gene names..."
 # More than one processed RNA-seq file can be supplied at the same time using wildcards
 # Each RNA-seq file will be read in as a column in a matirx of rnaseq data
 # NO quotes around the last argument if you are using wildcard
-echo "Step 4 Ouput y for PhiMRF (both intra and inter)..."
+echo "Step 3 Ouput y for PhiMRF (both intra and inter)..."
 for chrmA in "${chrms[@]}"
 do
-	#Rscript prep_y_for_MRF_intra.R "$chrmA" $homedir/intra/  $homedir/rnaseq/gene_names/  $homedir/rnaseq/rnaseq_*.txt
+	Rscript prep_y_for_MRF_intra.R "$chrmA" $homedir/intra/  $homedir/rnaseq/gene_names/  $homedir/rnaseq/rnaseq_*.txt
 	for chrmB in "${chrms[@]}"
 	do
-		#Rscript prep_y_for_MRF_inter.R "$chrmA" "$chrmB" $homedir/inter/  $homedir/rnaseq/gene_names/  $homedir/rnaseq/rnaseq_*.txt
+		Rscript prep_y_for_MRF_inter.R "$chrmA" "$chrmB" $homedir/inter/  $homedir/rnaseq/gene_names/  $homedir/rnaseq/rnaseq_*.txt
 	done
 done
 
